5428e90c895bb03107fbc2b508cad3d229c2f8f1
sulehosteldev <suleburasy@gmail.com>
2026-05-02 16:15:21 +0300

    Create Hero.js

---
+++ Hero.js
@@ -0,0 +1,23 @@
+export default function Hero() {
+
+  return (
+
+    <div style={{
+
+      textAlign: "center",
+
+      padding: 40,
+
+      background: "#e0f2ff"
+
+    }}>
+
+      <h1>Find Student Hostels Near Your Campus</h1>
+
+      <p>Search, compare and book easily</p>
+
+    </div>
+
+  )
+
+}
